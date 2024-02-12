1e7d57440f463d5c532a7f5b02ee6022b8083c90
Robert Ferguson <rferguson386@gmail.com>
2024-02-12 11:51:02 +0000

    fix problem with quiz function flow

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -19,12 +19,12 @@ const questions = [{
         Answer: 'Iago'
     },
     {
-        Question: 'When was the movie the Titanic released?',
-        Answer: 'Tin'
+        Question: 'When was the movie Titanic released?',
+        Answer: '1997'
     },
     {
         Question: 'What element is denoted by the chemical symbol Sn in the periodic table?',
-        Answer: '1997'
+        Answer: 'Tin'
     },
     {
         Question: "Which popular video game franchise has released games with the subtitles World At War and Black Ops?",
@@ -74,7 +74,7 @@ function startGame(event) {
     if (!hasNumber(userInputValue) && userInputValue) {
         userName = userInputValue;
         console.log('the game will start now');
-        displayQuestionSection()
+        displayGameContent()
     } else {
         alert("I think you can choose a better name than that! Please enter a username without any numeric characters");
     }
@@ -85,29 +85,13 @@ function hasNumber(myString) {
     return /\d/.test(myString);
 }
 
-function displayQuestionSection() {
+function displayGameContent() {
     introContent.classList.add('hide'); /* hide intro message */
     quizProgress.classList.remove('hide'); /* display info about current question number */
     quizQuestions.classList.remove('hide'); /* display quiz question */
-    setQuestionNumberDisplay();
-}
-
-function setQuestionNumberDisplay() {
-    currentQuestionCounter++;
-    /* Set the value of the message telling the user what question they are on */
-    let currentQuestionNumberDisplay = document.getElementById('question-number')
-    currentQuestionNumberDisplay.innerHTML = `<p>Question number ${currentQuestionCounter}</p>`;
-    /* check if the user should see the greeting message or not */
-    if (currentQuestionCounter === 1) {
-        greetings();
-    } else {
-        document.getElementById('first-question-greeting').classList.add('hide');
-    }
-    quizQuestions.classList.remove('hide'); /* display quiz questions */
-    quizFeedback.classList.add('hide'); /* hide answer feedback section */
-    answerFeedback.classList.add('hide'); /* hide specific answer feedback */
     runningScore.classList.remove('hide'); /* display running scores */
-    setQuestionText(currentQuestionCounter);
+    greetings();
+    setQuestionArea();
 }
 
 /* Use this only when displaying the first question in the quiz */
@@ -117,7 +101,23 @@ function greetings() {
     <p>Hello ${userName}, here is the first question:</p>`
 }
 
-/* Sets the question by fetching it from the answer property in the array index position
+/* Prepares the quiz question area so all sections are displayed, and can then have content written by subfunctions */
+function setQuestionArea() {
+    currentQuestionCounter++;
+    /* Set the value of the message telling the user what question they are on */
+    let currentQuestionNumberDisplay = document.getElementById('question-number')
+    currentQuestionNumberDisplay.innerHTML = `<p>Question number ${currentQuestionCounter}</p>`;
+    /* check if the user should see the greeting message or not */
+    if (currentQuestionCounter === 1) {} else {
+        document.getElementById('first-question-greeting').classList.add('hide');
+    }
+    quizQuestions.classList.remove('hide'); /* display quiz questions section */
+    quizFeedback.classList.add('hide'); /* hide answer feedback section */
+    answerFeedback.classList.add('hide'); /* hide specific answer feedback */
+    setQuestionText(currentQuestionCounter);
+}
+
+/* Sets the question by fetching it from the question property in the array index position
 which is 1 less than the current question number (by using the currentQuestionCounter variable) */
 function setQuestionText(questionNumber) {
     let currentQuestion = document.getElementById('current-question');
@@ -129,41 +129,48 @@ function setQuestionText(questionNumber) {
 function answerValidation(event) {
     event.preventDefault();
     let userAnswer = userAnswerForm.elements['userAnswer'].value;
-    /* check that the entry is not blank, this needs to be validated differently than the */
+    /* check that the entry is not blank */
     if (userAnswer) {
         userAnswers.push(userAnswer);
-        quizQuestions.classList.add('hide');
-        quizFeedback.classList.remove('hide');
-        answerFeedback.classList.remove('hide');
-        feedbackWriter(currentQuestionCounter);
+        setFeedbackArea();
     } else {
         alert("I didn't catch that, please enter the answer again and hit the submit button");
     }
 }
 
-function feedbackWriter(questionNumber) {
+/* Prepares the answer feedback so all sections are displayed, and can then have content written by subfunctions */
+function setFeedbackArea() {
+    quizQuestions.classList.add('hide'); /* hide quiz questions section */
+    quizFeedback.classList.remove('hide'); /* display answer feedback section */
+    answerFeedback.classList.remove('hide'); /* display specific answer feedback - still to be written at this point */
+    setFeedbackText(currentQuestionCounter);
+    document.getElementById('next-question').addEventListener('click', setQuestionArea);
+}
+
+/* Sets the question by fetching it from the answer property in the array index position
+which is 1 less than the current question number (by using the currentQuestionCounter variable) */
+function setFeedbackText(questionNumber) {
     let answerIndex = questionNumber - 1;
     let feedback = document.getElementById('feedback');
     console.log(questions[0].Answer);
     console.log('answer index is ', answerIndex, 'question answer is ', questions[answerIndex].Answer, 'user answer is ', userAnswers[answerIndex]);
     if (questions[answerIndex].Answer == userAnswers[answerIndex]) {
         feedback.innerHTML = `
-        <p>Congratulations ${userName}, you got that question correct</p>`
+        <p>Congratulations ${userName}, you got that question correct</p>`;
         addCorrectScore();
     } else {
         feedback.innerHTML = `
-        <p>I'm sorry ${userName}, you got that question wrong, the answer should have been ${questions[answerIndex].Answer}</p>`
+        <p>I'm sorry ${userName}, you got that question wrong, the answer should have been ${questions[answerIndex].Answer}</p>`;
         addIncorrectScore();
     }
-    document.getElementById('next-question').addEventListener('click', setQuestionNumberDisplay);
 }
 
 function addCorrectScore() {
-    corectScoreCount++
-    document.getElementById('correct-score-tally').innerHTML = correctScoreCount, ' correct answers';
+    correctScoreCount++;
+    document.getElementById('correct-score-tally').innerHTML = correctScoreCount + ' correct answers';
 }
 
 function addIncorrectScore() {
-    incorectScoreCount++
-    document.getElementById('incorrect-score-tally').innerHTML = correctScoreCount, 'incorrect answers';
+    incorrectScoreCount++;
+    document.getElementById('incorrect-score-tally').innerHTML = incorrectScoreCount + ' incorrect answers';
 }
